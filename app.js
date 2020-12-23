89974cb96fed7fd70774a6dc17ded1a40ccf0136
gkf <gkf2103@gmail.com>
2020-12-23 17:40:39 -0500

    initial JS functionality gkf & jas collab

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+}); 
+
+
 $('.burger').on('click', function() {
     $(this).toggleClass('active');
     $('.overlay').toggleClass('burger-open');
@@ -8,3 +12,163 @@ $('nav a').on('click', function() {
     $('.burger').removeClass('active');
     $('overlay').removeClass('burger-open');
 });
+
+
+
+
+
+
+
+
+// step 1: use maps javascript to create a basic map using the latitude and longitude coordinates for which ever location in order to test whether the map zeros into the location. the let variable MUST be changed depending on the location in oder to best track which static location we are performing the test on.  
+
+let infoWindow;
+let autocomplete;
+let marker= [];
+let charitiesApi= [];
+let charityGeoCodeResults = [];
+//charity navigator
+$(document).ready(function(){
+var orgname = $('#orgname'); 
+//*  CHARITY NAVIGATOR 
+var cnAPIID = "29acb795";
+var cnAPIKEY = "04b276e51b8c538e2a38bb533518a83d";
+var navigatorURL = "https://api.data.charitynavigator.org/v2/Organizations?app_id=" + cnAPIID + "&app_key=" + cnAPIKEY; 
+function runQuery(cnURL){
+    $.ajax({url: cnURL,
+    method: "GET"})
+    .then(function(cnData){
+        
+        orgname.text(cnData[0].charityName);
+        for (let i= 0; i < cnData.length; i++) {
+            if(!cnData[i].mailingAddress.streetAddress1.includes('PO')){
+                charitiesApi.push(cnData[i])
+                
+            }
+        }
+        var geoCodeURL = 'https://maps.google.com/maps/api/geocode/json?key=&address='
+        for (let j= 0; j < 5; j++) {
+            var address= charitiesApi[j].mailingAddress.streetAddress1 + "," + charitiesApi[j].mailingAddress.city + "," + charitiesApi[j].mailingAddress.stateOrProvince + "," + charitiesApi[j].mailingAddress.postalCode;
+            var charitiesGeoCode=  geoCodeURL + address;
+            $.ajax({url: charitiesGeoCode,
+            method: "GET"})
+            .then(function(geoLocation) {
+                
+                charityGeoCodeResults.push(geoLocation)
+            });
+
+        }
+        console.log("hi", charityGeoCodeResults)
+        for(let k = 0; k < charityGeoCodeResults.length; k++){
+            let place = charityGeoCodeResults[k];
+            console.log(place.geometry.location)
+            new google.maps.Marker({
+                map,
+
+                position: place.geometry.location,
+
+            });
+        }
+        
+
+    });
+
+
+}
+
+
+// Google map
+let map;
+
+const sanFran= { lat: 37.7576171, lng: -122.5776844 }
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: sanFran,
+        zoom: 10,
+
+    });
+    // Info windows will shows the list of results from the search
+    infoWindow = new google.maps.InfoWindow({
+        content: document.getElementById("info-content"),
+    });
+    // for the search box to help the user complete their cities search
+    autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("autocomplete"),
+        {
+            types: ["(cities)"],
+            componentRestrictions: countryRestrict,
+        }
+    );
+
+    
+   //static location search     
+    const service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({location: sanFran, radius: 16093.4, type: "hospital", keyword: "bloodbank", setting: "open now"},function (results, status, pagination){
+        console.log(results)
+        for(let i = 0; i < results.length; i++){
+            let place = results[i]
+            new google.maps.Marker({
+                map,
+                title: place.name,
+                position: place.geometry.location,
+            });
+        }
+    })
+} 
+
+
+function getZipFood() {
+    $("#bloodButton").css("display","none")
+    $("#foodButton").css("display","none")
+    $("#timeButton").css("display","none")
+    $("#modalBox").css("display", "grid")
+    $("#modalBox").html("")
+    $("#modalBox").html("")
+
+    
+} 
+
+function getZipBlood() {
+    $("#bloodButton").css("display","none")
+    $("#foodButton").css("display","none")
+    $("#timeButton").css("display","none")
+    $("#modalBox").css("display", "grid")
+    $("#modalBox").html("")
+    $("#modalBox").html("")
+    
+   
+}
+
+function getZipTime() {
+    $("#bloodButton").css("display","none")
+    $("#foodButton").css("display","none")
+    $("#timeButton").css("display","none")
+    $("#modalBox").css("display", "grid")
+    $("#modalBox").html("")
+    $("#modalBox").html("")
+    
+   
+}
+
+
+
+
+
+$("#bloodButton").on("click",getZip);
+$("#foodButton").on("click", getZip);
+$("#timeButton").on("click", getZip);
+
+
+
+
+
+
+
+
+
+
+});
+    
+
+
+            
